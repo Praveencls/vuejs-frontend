a44787b784fd2450deb88e24e85ed3b335171ced
praveencls <praveencls@yahoo.com>
2026-02-20 16:05:58 +0530

    initial commit of vue 3 project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Vue 3 Frontend</h1>
+    <HelloApi />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelloApi from './components/HelloApi.vue';
+</script>
---
+++ src/components/HelloApi.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import api from "../services/api";
+
+const message = ref("");
+
+onMounted(async () => {
+  try {
+    const res = await api.get("/hello"); // Spring Boot endpoint
+    message.value = res.data;
+  } catch (err) {
+    console.error(err);
+    message.value = "API call failed";
+  }
+});
+</script>
+<template>
+  <div>
+    <h2>Spring Boot API says:</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
