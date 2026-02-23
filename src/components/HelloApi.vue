77014d6c6759f0a3460a8bfa9080044c68a274fa
praveencls <praveencls@yahoo.com>
2026-02-23 23:48:20 +0530

    test fe deploy from github

--- src/components/HelloApi.vue
+++ src/components/HelloApi.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
-import api from "../services/api";
+import { ref, onMounted } from 'vue'
+import api from '../services/api'
 
-const message = ref("");
+const message = ref('')
 
 onMounted(async () => {
   try {
-    const res = await api.get("/hello"); // Spring Boot endpoint
-    message.value = res.data;
+    const res = await api.get('/hello') // Spring Boot endpoint1
+    message.value = res.data
   } catch (err) {
-    console.error(err);
-    message.value = "API call failed";
+    console.error(err)
+    message.value = 'API call failed'
   }
-});
+})
 </script>
 <template>
   <div>
